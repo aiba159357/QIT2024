a3661fd78f8de54bab95931c5cd4d23027cb3cf1
Umitbeluly Aibarys <umitbekulyajbarys@gmail.com>
2024-10-31 16:23:53 +0500

    exercise31

---
+++ exercises/31.10/index.js
@@ -0,0 +1,121 @@
+
+//  var x = 30;
+
+//  var y = 40;
+
+
+//  var z = x + у; // Арифметикалық операция
+
+//  console.log(z);
+
+
+
+
+//  let a = 20 ;
+
+//  let b = 25 // тағайындау/меншіктеу
+
+
+
+// // console.log(a - b) ; // Азайту операциясы
+
+// let a = 10;
+// let b = 60;
+// let total = a*b;   ////kobeutu operasiatsysy
+
+// console.log(total);   ///600
+
+
+
+//  let c = 50;
+//  let d = 3;
+
+//  console.log(c / d);  
+
+
+// let num1 = 50;
+// let num2= 30;
+
+// console.log(num1 % num2);  //20 kalduk
+
+
+
+
+
+// let num1 = 53;
+//  if (num1 % 2 ==0) {
+//     console.log(num1, "jup san");
+//  } else{
+//     console.log(num1, "tag san");
+//  }
+
+// let num = 20;
+//  num++; //num = num+1
+
+//  console.log(num)
+
+
+//  num--;   //num = num-1
+//  console.log(num);
+ 
+
+
+
+
+
+
+// let num = 20;
+//  num +=5;
+
+//  console.log(num);
+
+
+// let num = 30;
+
+// num -=6;
+
+// console.log(num);
+
+
+
+
+
+
+
+// let x =159;
+// x*=3;
+
+// console.log(x);
+
+
+// let num1 = 300;
+// let num2 = 3;
+
+// num1 %= num2;
+// console.log(num1);
+
+// let a = 5;
+// let b = 3;
+// let c = 10;
+// let d = 2;
+// let e = 2;
+
+// let total = (a+b) * c - d / e;
+
+// console.log(total);
+
+
+
+
+
+let a = 3 ;
+let b = 2;
+let c = 1;
+let z = 5;
+ 
+
+let total = ((a*b) /c) / (z-3);
+
+
+
+console.log(total %2 );
